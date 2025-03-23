34e077baae37e0aa1b954d01ab970d2ee8825153
punnawich5454 <ppopokjf1223@gmail.com>
2025-03-23 13:19:00 +0700

    Create ep1 - Copy.py

---
+++ ep1 - Copy.py
@@ -0,0 +1,13 @@
+#string = ข้อความ ตัวอักษร '',""
+#number = integer , float
+#boolean = true , false
+
+print('hello')
+print("ice")
+print(15)
+print(15.5)
+print(True)
+print(False)
+
+print("punnawich \n" + "pengsuk") #ลงมาหนึ่งบรรทัด
+print("punnawich \t" + "pengsuk")#เว้นบรรทัด
